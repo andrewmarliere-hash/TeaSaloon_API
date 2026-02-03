59cb5ec3170a060b990e3eb9f9bc72d565edfe35
andrew <andrew@SPB25-14>
2026-02-03 15:26:52 +0100

    Génération des controlleurs

---
+++ TeaSaloon_API/Controllers/OrderLinesController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TeaSaloon_API.Data;
+using TeaSaloon_API.Models;
+
+namespace TeaSaloon_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderLinesController : ControllerBase
+    {
+        private readonly TeaSaloonContext _context;
+
+        public OrderLinesController(TeaSaloonContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/OrderLines
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<OrderLine>>> GetOrderLines()
+        {
+            return await _context.OrderLines.ToListAsync();
+        }
+
+        // GET: api/OrderLines/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<OrderLine>> GetOrderLine(long id)
+        {
+            var orderLine = await _context.OrderLines.FindAsync(id);
+
+            if (orderLine == null)
+            {
+                return NotFound();
+            }
+
+            return orderLine;
+        }
+
+        // PUT: api/OrderLines/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOrderLine(long id, OrderLine orderLine)
+        {
+            if (id != orderLine.OrderLineID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(orderLine).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderLineExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/OrderLines
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<OrderLine>> PostOrderLine(OrderLine orderLine)
+        {
+            _context.OrderLines.Add(orderLine);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetOrderLine", new { id = orderLine.OrderLineID }, orderLine);
+        }
+
+        // DELETE: api/OrderLines/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrderLine(long id)
+        {
+            var orderLine = await _context.OrderLines.FindAsync(id);
+            if (orderLine == null)
+            {
+                return NotFound();
+            }
+
+            _context.OrderLines.Remove(orderLine);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool OrderLineExists(long id)
+        {
+            return _context.OrderLines.Any(e => e.OrderLineID == id);
+        }
+    }
+}
